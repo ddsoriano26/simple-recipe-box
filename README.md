b85adb58a97d2ecd0cf16737c8ff096cc4898f6e
Denise Soriano <denisedsoriano@gmail.com>
2026-08-20 20:45:41 +0800

    Add home page and MealCard

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Simple Recipe Box
 
 A simple web app for me to practice and familiarize with VueJS concepts and APIs.
+
+## Functionality
+
+- [x] Show recipes list in first page
+- [ ] Clicking on a recipe shows the details of the recipe
+- [ ] Show only favorites
---
+++ src/components/MealCard.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import type { Meal } from '@/utils/types'
+
+defineProps<{
+  meal: Meal
+}>()
+</script>
+
+<template>
+  <main>
+    <img v-bind:src="meal.strMealThumb" />
+    <div class="name">
+      <text>{{ meal.strMeal }}</text>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  border: 1px solid black;
+  border-radius: 10px;
+  overflow: auto;
+  height: 200px;
+  width: 300px;
+}
+
+img {
+  width: 100%;
+  height: auto;
+  max-height: 80%;
+  object-fit: cover;
+}
+
+.name {
+  display: flex;
+  width: 100%;
+  height: auto;
+  max-height: 20%;
+  justify-content: center;
+  align-items: center;
+  padding: 5px 10px;
+  align-content: center;
+}
+</style>
